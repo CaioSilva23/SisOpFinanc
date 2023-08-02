1428c27b9ccf96a8a161b8fc27e6eda34e3b6bf8
caio silva <caio_ceac23@hotmail.com>
2023-08-02 09:21:50 -0300

    .env-exemple update

--- backend/env-exemple.py
+++ backend/env-exemple.py
@@ -1,21 +1,21 @@
-# # EMAIL'
-# email_host=''
-# email_port=''
-# email_user=''
-# email_password=''
+# # EMAIL
+# email_host=
+# email_port=
+# email_user=
+# email_password=
 
 # # jwt
-# SECRET_KE=''
+# SECRET_KE=
 
 # # redis
-# redis_host=''
-# redis_port=0
-# redis_db=0
+# redis_host=
+# redis_port=
+# redis_db=
 
 
 # # postgres
-# db_host=''
-# db_name=''
-# db_user=''
-# db_password=''
-# port=
+# POSTGRES_USER=
+# POSTGRES_PASSWORD=
+# POSTGRES_DB=
+# DB_HOST=
+# DB_PORT=
